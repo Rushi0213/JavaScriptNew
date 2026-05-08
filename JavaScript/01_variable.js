a91d5ac09eb9af7012fa03b9b79602bed9dad0dc
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-08 23:21:47 +0530

    variables in js

---
+++ JavaScript/01_variable.js
@@ -0,0 +1,13 @@
+const a="rushi";
+// a="raja"
+// we can not usse this value again in constant
+
+let b="ram";
+// b="ramrav"
+// here we can use it in let type
+
+var c="sham";
+// c="rajaram"
+// this is same as a let but it is not moslty use in js,it is not safely
+// it is issue wit  block scope and functio scope
+console.table([a,b,c])
