fc9f0ecfca1c1c5a2483c1c7d2940facec248ce1
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-09 22:54:56 +0530

    datatypes in js

---
+++ JavaScript/02_datatypes.js
@@ -0,0 +1,28 @@
+
+//datatypes in javascript
+
+//number=>6
+//string=>"rushi"
+//boolean=>true/false
+//null=> it it object
+//undefined=> it ids not defined then showing in browser
+//bigint=> it use when value is very long
+//symbol=> used for unique
+
+//thse all datatype use by const,var,let=> they are not write like in java or any other language
+
+//ex
+
+let n=6
+let name="rushi"
+let  login=true
+let state=null
+let  city;
+
+console.table([
+    typeof n,
+    typeof name,
+    typeof login,
+    typeof state,
+    typeof city
+]);
