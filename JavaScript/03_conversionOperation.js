621a3e54b67c1fae7881f5f87ce5751f2770a3fd
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-09 22:55:44 +0530

    conversion operations in js

---
+++ JavaScript/03_conversionOperation.js
@@ -0,0 +1,49 @@
+
+// this is number to string convesation
+
+let numbers=10
+
+let newnumber=String(numbers)
+
+// console.log(numbers);
+// console.log(typeof(numbers));
+
+
+// console.log(newnumber);
+// console.log(typeof(newnumber));
+
+
+//this is the conversation in strin to number
+
+let  strings="rushi"
+let newstrings=Number(strings)
+
+// console.log(strings);
+// console.log(typeof(strings));
+
+// console.log(newstrings);
+// console.log(typeof(newstrings));
+
+
+
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(+true);
+// console.log(+"");
+
+
+let num1,num2,num3;
+num1=num2=num3=2+2
+
+
+let num=111
+num++
+// console.log(num);
+
+
+
+
+
+
+
+
