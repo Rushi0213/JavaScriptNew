69a084160eec93310da4fba30cc500a10d079121
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-10 22:49:02 +0530

    string in js

---
+++ JavaScript/07_string.js
@@ -0,0 +1,49 @@
+
+const name="rushikesh";
+const depatment="IT";
+console.log(`hello my name is ${name} and i am graduate in ${depatment}`);
+
+const gamename= new String("RushIkesh");
+
+
+console.log(gamename[0]);
+
+
+console.log(gamename);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.toLowerCase());
+console.log(gamename.charAt(3));
+
+console.log(gamename.indexOf('k'));
+console.log(gamename.substring(0,4));
+console.log(gamename.slice(-8,4));
+
+
+const newstring="  ramu ";
+console.log(newstring);
+console.log(newstring.trim());
+
+
+const url= "http://rushi/main/drive/rushi deshmukh";
+
+console.log(url.replace(" ","-"));
+
+console.log(url.includes("rushi"));
+
+const newone="rushi-deshmukh-final";
+
+console.log(newone.split("-"));
+
+
+
+
+
+
+
+
+
+
+
+
+
